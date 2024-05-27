8f40d4f7ab4b56a4b30540181dc2aae929c7d512
William <williamfs133@gmail.com>
2024-05-27 13:36:59 -0300

    change: django-admin setup

--- webpage/blog/models/post.py
+++ webpage/blog/models/post.py
@@ -11,6 +11,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     updated_on = models.DateField(auto_now=True)
+    content = models.TextField()
     created_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
